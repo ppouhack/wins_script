6a9454949e9997e1bd5fe0827a5372bbd9d170fe
ssdh1009 <ssdh1009@Wins21.co.kr>
2020-10-29 11:53:50 +0900

    feat: brief, translation
    xml file important information brief
    brief file translation korean

--- trendmicro/trendmicro.py
+++ trendmicro/trendmicro.py
@@ -5,7 +5,7 @@ from googletrans import Translator
 import configparser
 from ast import literal_eval
 import argparse
-
+from time import sleep
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -21,6 +21,10 @@ class trendMicro():
         if self.info:
             for info in self.info:
                 self.download(info, self.session)
+                if args.translation:
+                    self.brief(info,True)
+                else:
+                    self.brief(info, False)
 
     def login(self):
         """ return the trendmicro login session
@@ -134,11 +138,210 @@ class trendMicro():
 
         print(f"> download complete\n")
 
-text = "python trendmicro.py -t -s (cve_id or tsl_id)"
-arg_parser = argparse.ArgumentParser(description=text)
-arg_parser.add_argument('--search', '-s', required=False, help='search cve_id or tsl_id')
-arg_parser.add_argument('--file', '-f', required=False, help='search file cve_id or tsl_id')
-arg_parser.add_argument('--translation', '-t', action='store_true')
-args = arg_parser.parse_args()
+    def google_translation(self,data):
+        """ english string translation korean string
 
-tm = trendMicro(args)
+        Args:
+            data: english string
+
+        Returns:
+            korean string
+        """
+        try:
+            sleep(0.1)
+            translator = Translator()
+            return translator.translate(data, src="en", dest="ko").text
+        except Exception as e:
+            return "[!] translator error"
+
+    def brief(self,info,flag):
+        """ brief xml file
+
+        Args:
+            info : validate vulnerabilities list(dictionary)
+            flag : if flag is true, call google_translation
+
+        Returns:
+            translation.txt
+        """
+        if flag:
+            print(f"# translation brief {info['title']}")
+        else:
+            print(f"# brief {info['title']}")
+        filename = f"{info['title']}/{info['tsl_id']}.xml"
+
+        xml = open(filename, "r")
+        xmlObj = BeautifulSoup(xml, 'html.parser')
+
+        with open(f"{info['title']}/brief.txt", 'w', encoding='UTF8') as w:
+
+            # Summary Information
+            print(f"# 0.Summary Information", file=w)
+            print(f"", file=w)
+
+            try:
+                for shortname in xmlObj.identity.findAll('shortname'):
+                    title = shortname.text
+                    print(f"> title", file=w)
+                    print(f"{title}", file=w)
+                    print(f"", file=w)
+
+            except Exception as e:
+                print(f"> title", file=w)
+                print(f"No", file=w)
+                print(f"", file=w)
+
+            try:
+                for vendor_advisory in xmlObj.identity.findAll('vendor_advisory'):
+                    vendor_advisory_name = vendor_advisory.get('name')
+                    vendor_advisory_url = vendor_advisory.get('url')
+
+                    print(f"> vendor_advisory_name", file=w)
+                    print(f"{vendor_advisory_name}", file=w)
+                    print(f"",file=w)
+
+                    print(f"> vendor_advisory_url", file=w)
+                    print(f"{vendor_advisory_url}", file=w)
+                    print(f"", file=w)
+
+            except Exception as e:
+                print(f"> vendor_advisory_name", file=w)
+                print(f"No", file=w)
+                print(f"", file=w)
+
+            try:
+                for cve in xmlObj.identity.findAll('cve'):
+                    cve_id = cve.get('id')
+                    print(f"> CVE ID", file=w)
+                    print(f"{cve_id}", file=w)
+                    print(f"", file=w)
+
+            except Exception as e:
+                print(f"> CVE ID", file=w)
+                print(f"No", file=w)
+
+            try:
+                for discovereradvisory in xmlObj.identity.findAll('discovereradvisory'):
+                    discovereradvisory_name = discovereradvisory.get('name')
+                    discovereradvisory_url = discovereradvisory.get('url')
+
+                    print(f"> discovereradvisory_name", file=w)
+                    print(f"{discovereradvisory_name}", file=w)
+                    print(f"", file=w)
+
+                    print(f"> discovereradvisory_url", file=w)
+                    print(f"{discovereradvisory_url}", file=w)
+                    print(f"", file=w)
+
+            except Exception as e:
+                print(f"> discovereradvisory_name", file=w)
+                print(f"No", file=w)
+                print(f"", file=w)
+
+                print(f"> discovereradvisory_url", file=w)
+                print(f"No", file=w)
+                print(f"", file=w)
+
+            try:
+                print(f"> product_data", file=w)
+
+                for vendor in xmlObj.affectedproducts.affecteddirectly.findAll('vendor'):
+                    vendor_name = vendor.get('name')
+                    for product in vendor.findAll('product'):
+                        product_name = product.get('name')
+                        for version in product.findAll('version'):
+                            version_name = version.get('name')
+                            product_data = f"{vendor_name} {product_name} {version_name}"
+
+                            print(f"{product_data}", file=w)
+
+            except Exception as e:
+                print(f"No", file=w)
+
+            print(f"", file=w)
+
+            try:
+                print(f"> public exploit", file=w)
+                for url in xmlObj.publicexploits.description.findAll('url'):
+                    publicexploits_url = url.text
+                    print(f"{publicexploits_url}", file=w)
+
+            except Exception as e:
+                print(f"No", file=w)
+
+            print(f"", file=w)
+
+            # Brief Description
+            try:
+                print(f"# 1.Brief Description", file=w)
+                for para in xmlObj.identity.description.findAll('para'):
+                    identity_paras = para.text
+                    identity_paras = identity_paras.replace("\n"," ")
+                    print(f"{identity_paras}", file=w)
+                    print(f"", file=w)
+                    if flag:
+                        print(f"{self.google_translation(identity_paras)}", file=w)
+                        print(f"", file=w)
+
+            except Exception as e:
+                print(f"No", file=w)
+            print(f"", file=w)
+
+            # Detail Analysis
+            try:
+                print(f"# 2.Detail Analysis", file=w)
+                for para in xmlObj.mechanism.description.findAll('para'):
+                    if para.findAll('code'):  # <code>가 포함되어 있는 경우 건너뜀
+                        for code in para.findAll('code'):
+                            mechanism_code = code.text
+                            print(f"{mechanism_code}", file=w)
+                            print(f"", file=w)
+                    else:
+                        mechanisms_paras = para.text
+                        mechanisms_paras = mechanisms_paras.replace("\n", " ")
+
+                        print(f"{mechanisms_paras}", file=w)
+                        print(f"", file=w)
+
+                        if flag:
+                            print(f"{self.google_translation(mechanisms_paras)}", file=w)
+                            print(f"", file=w)
+
+            except Exception as e:
+                print(f"No", file=w)
+
+            # Detection
+            try:
+                print(f"# 3.Detection", file=w)
+
+                for para in xmlObj.attackdetection.genericattacks.findAll('para'):
+                    global attackdetection_data
+                    if para.findAll('code'):
+                        for code in para.findAll('code'):
+                            attack_detection_code = code.text
+                            print(f"{attack_detection_code}", file=w)
+                            print(f"", file=w)
+                    else:
+                        attack_detection_paras = para.text
+                        attack_detection_paras = attack_detection_paras.replace("\n", " ")
+
+                        print(f"{attack_detection_paras}", file=w)
+                        print(f"", file=w)
+
+                        if flag:
+                            print(f"{self.google_translation(attack_detection_paras)}", file=w)
+                            print(f"", file=w)
+
+            except Exception as e:
+                print(f"No", file=w)
+
+        print(f"> brief complete")
+
+if __name__ == "__main__":
+    text = "python trendmicro.py -t -s (cve_id or tsl_id)"
+    arg_parser = argparse.ArgumentParser(description=text)
+    arg_parser.add_argument('--search', '-s', required=True, help='search cve_id or tsl_id')
+    arg_parser.add_argument('--translation', '-t', action='store_true', help='brief translation')
+    args = arg_parser.parse_args()
+
+    tm = trendMicro(args)
